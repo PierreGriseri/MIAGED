859555fcc198d5b64ea4ad4198fb30488fd0f976
PierreGriseri <pierre.griseri@gmail.com>
2021-12-01 16:39:02 +0100

    On avance petit à petit

--- lib/screens/clothes_list.dart
+++ lib/screens/clothes_list.dart
@@ -1,5 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:miaged/assets/background.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:miaged/screens/user_profile.dart';
+
 
 class ClothesList extends StatefulWidget {
 
@@ -14,6 +18,27 @@ class _ClothesListState extends State<ClothesList> {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+        body: AnnotatedRegion<SystemUiOverlayStyle>(
+            value: SystemUiOverlayStyle.light,
+            child: GestureDetector(
+              onTap: () => FocusScope.of(context).unfocus(),
+              child: Stack(
+                children: [
+                  const Background(),
+                  Container(
+                    child: buildListView(context),
+                  ),
+                ]
+              )
+            )
+        )
+    );
+  }
+
+
+
+  Widget buildListView(context) {
     final Stream<QuerySnapshot> _objectStream = FirebaseFirestore.instance.collection('object').snapshots();
 
     return StreamBuilder<QuerySnapshot>(
@@ -30,15 +55,28 @@ class _ClothesListState extends State<ClothesList> {
         return ListView(
           children: snapshot.data!.docs.map((DocumentSnapshot document) {
             Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-            return Text(
-              data['titre'] + ", " + data['marque']
-              //title: Text(data['titre']),
-              //subtitle: Text(data['marque']),
+            return ListTile(
+              title: Text(data['titre']),
+              subtitle: Text(data['marque']),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => UserProfile()));
+              },
             );
           }).toList(),
         );
       },
     );
   }
-
 }
+
+//FOR EACH POUR AFFICHAGE
+/*FirebaseFirestore.instance
+    .collection('users')
+.get()
+    .then((QuerySnapshot querySnapshot) {
+querySnapshot.docs.forEach((doc) {
+print(doc["first_name"]);
+});
+});*/
